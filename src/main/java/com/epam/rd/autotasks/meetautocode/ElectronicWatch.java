2a1604a78b85fd113eb82b6014d2aeec8ab1fae6
Anastasiya <test@mail.test>
2022-10-25 00:16:29 +0300

    task implementation

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int seconds = scanner.nextInt();
+        int input = scanner.nextInt();
 
+        int seconds = input % 60;
+        int minutes = input / 60 % 60;
+        int hours = input / 3600 % 24;
 
+        System.out.printf("%d:%02d:%02d",hours, minutes, seconds);
     }
 }
